be2e560fa903365ed64b7982f7e379e462334596
Vladimir Kidyaev <krios2146@gmail.com>
2023-02-07 18:42:02 +0700

    feat: now all functions associated with user input are in util/input.py

---
+++ src/util/input.py
@@ -0,0 +1,21 @@
+from util.validation import is_valid_number, is_valid_comma_separated_list_of_numbers
+
+
+def ask_number():
+    input_str = input()
+
+    while not is_valid_number(input_str):
+        print("Invalid input string. Please enter a number: ")
+        input_str = input()
+
+    return float(input_str)
+
+
+def ask_comma_separated_list_of_numbers():
+    xi_input = input()
+
+    while not is_valid_comma_separated_list_of_numbers(xi_input):
+        print("Invalid input string. Please enter a comma-separated list of numbers: ")
+        xi_input = input()
+
+    return [float(num) for num in xi_input.split(',')]
