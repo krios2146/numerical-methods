6625ab0b5616367a2286a1bea0bbba101b0e6d1d
Vladimir Kidyaev <krios2146@gmail.com>
2023-02-07 19:18:45 +0700

    feat: implement algorithm that calculates absolute error

--- src/practices/Practice7To8.py
+++ src/practices/Practice7To8.py
@@ -19,27 +19,37 @@ class Practice7To8:
         print("Enter the x values separated by comma: ")
         self.x = ask_comma_separated_list_of_numbers()
 
+        # TODO: a should be in range of interpolation
         print("Enter the a value: ")
         self.a = ask_number()
 
         self.complete_practice()
 
-    def calculate_polynomial_at_given_point(self, a):
+    def complete_practice(self):
+        polynomial_value = self.calculate_polynomial_at_given_point(self.a)
+        print(f"Value of f(x) at the {self.a} calculated using Lagrange polynomial = {polynomial_value}")
+
+        absolute_error = self.calculate_absolute_error_at_given_point(self.a)
+        print(f"Absolute error at the {self.a} = {absolute_error}")
+
+    def calculate_polynomial_at_given_point(self, point):
         polynomial = 0
 
         for xi in self.x:
             polynomial_coefficient = 1
             for xj in self.x:
                 if xj != xi:
-                    polynomial_coefficient *= (a - xj) / (xi - xj)
+                    polynomial_coefficient *= (point - xj) / (xi - xj)
 
-            polynomial += polynomial_coefficient * self.calculate_y_value(xi)
+            polynomial += polynomial_coefficient * self.calculate_y_value_at_given_point(xi)
 
         return polynomial
 
-    def calculate_y_value(self, x):
-        return math.log(math.pow(x, (self.pow_numerator / self.pow_denominator)))
+    def calculate_y_value_at_given_point(self, point):
+        return math.log(math.pow(point, (self.pow_numerator / self.pow_denominator)))
 
-    def complete_practice(self):
-        polynomial_value_at_point = self.calculate_polynomial_at_given_point(self.a)
-        print(f"Value of f(x) at the {self.a} calculated using Lagrange polynomial = {polynomial_value_at_point}")
+    def calculate_absolute_error_at_given_point(self, point):
+        polynomial_value = self.calculate_polynomial_at_given_point(point)
+        y_value = self.calculate_y_value_at_given_point(point)
+
+        return abs(polynomial_value - y_value)
