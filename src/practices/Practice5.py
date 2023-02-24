9eb5d19131c54d79ac294ed41c31fe7ef076a14d
Vladimir Kidyaev <krios2146@gmail.com>
2023-02-24 19:10:46 +0700

    refactor: rename coefficients from c_i to a_i for readability

--- src/practices/Practice5.py
+++ src/practices/Practice5.py
@@ -5,12 +5,12 @@ class Practice5:
     initial_approximation = []
     calculation_accuracy = 0.0001
 
-    c_1 = [0.23, 0.21, 0.06, -0.34]
-    c_2 = [0.05, 0, 0.23, 0.12]
-    c_3 = [0.35, -0.27, 0, -0.05]
-    c_4 = [0.12, -0.43, 0.34, -0.22]
+    a_1 = [0.23, 0.21, 0.06, -0.34]
+    a_2 = [0.05, 0, 0.23, 0.12]
+    a_3 = [0.35, -0.27, 0, -0.05]
+    a_4 = [0.12, -0.43, 0.34, -0.22]
 
-    coefficients = [c_1, c_2, c_3, c_4]
+    coefficients = [a_1, a_2, a_3, a_4]
 
     def initiate_practice(self):
         if self.is_convergence_condition_satisfied():
